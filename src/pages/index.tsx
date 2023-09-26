ea44024e7f0b55fe5f5f18a86dfea055ea09dd8a
yoonme44 <yoonmem86@gmail.com>
2023-09-26 10:50:30 +0630

    create pages update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import 'animate.css';
 import Instructor from '@/components/Instructor'
 export default function Home() {
   return (
-    <div>
+    <div className='w-full right-0 animate__animated animate__bounceInDown animate__duration-8000 animate__fill-both'>
       <div>
         <i className='text-[48px] text-black font-semibold'>
           <PageTitle title="Plan your" />
@@ -22,19 +22,15 @@ export default function Home() {
         </i>
       </div>
       <div className='absolute top-0 right-0'>
-        <div className='w-full right-0 animate__animated animate__bounceInDown animate__duration-2000 animate__fill-both border-t-[80px] border-t-transparent
-    border-l-[200px] border-l-rose-600
-    border-b-[80px] border-b-transparent'>
+        {/* <div className='w-full right-0 animate__animated animate__bounceInDown animate__duration-2000 animate__fill-both border-t-[80px] border-t-transparent border-l-[200px] border-l-rose-600 border-b-[80px] border-b-transparent'>
           <Image
             src="/images/sample.jpg"
             alt="sample.jpg"
             width={500}
             height={100}
           />
-        </div>
+        </div> */}
       </div>
-      
-
     </div>
 
   )
