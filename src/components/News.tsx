aef5c3053ad5c7d6963b950b4685ca962f912504
Arkar <arkarmin.mi@gmail.com>
2023-09-27 10:19:38 +0630

    chore: news

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -33,6 +33,90 @@ const News = () => {
             </div>
           </div>
         </div>
+        <div className="flex flex-col lg:md:flex-row w-full min-h-5  mx-auto my-4 border shadow-lg rounded-3xl bg-[#F3F3F3]">
+          <Image
+            src="/statics/images/news1.jpg"
+            width={350}
+            height={300}
+            alt="news-img"
+            className="rounded-l-3xl flex-grow-3 hidden lg:block"
+          />
+          <Image
+            src="/statics/images/news1.jpg"
+            width={1000}
+            height={10}
+            alt="news-img"
+            className="rounded-t-xl flex-grow-3  lg:hidden block mb-4"
+          />
+
+          <div className="flex flex-col justify-center">
+            <p className="lg:p-8 px-4  w-full lg:md:text-sm text-xs">
+              Many young Myanmar people, including myself, go abroad to work in
+              order to change their lives...
+            </p>
+            <div className="w-full flex justify-end pb-2">
+              <div className="w-contain py-2">
+                <MoreBtn />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col lg:md:flex-row w-full min-h-5  mx-auto my-4 border shadow-lg rounded-3xl bg-[#F3F3F3]">
+          <Image
+            src="/statics/images/news1.jpg"
+            width={350}
+            height={300}
+            alt="news-img"
+            className="rounded-l-3xl flex-grow-3 hidden lg:block"
+          />
+          <Image
+            src="/statics/images/news1.jpg"
+            width={1000}
+            height={10}
+            alt="news-img"
+            className="rounded-t-xl flex-grow-3  lg:hidden block mb-4"
+          />
+
+          <div className="flex flex-col justify-center">
+            <p className="lg:p-8 px-4  w-full lg:md:text-sm text-xs">
+              Many young Myanmar people, including myself, go abroad to work in
+              order to change their lives...
+            </p>
+            <div className="w-full flex justify-end pb-2">
+              <div className="w-contain py-2">
+                <MoreBtn />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col lg:md:flex-row w-full min-h-5  mx-auto my-4 border shadow-lg rounded-3xl bg-[#F3F3F3]">
+          <Image
+            src="/statics/images/news1.jpg"
+            width={350}
+            height={300}
+            alt="news-img"
+            className="rounded-l-3xl flex-grow-3 hidden lg:block"
+          />
+          <Image
+            src="/statics/images/news1.jpg"
+            width={1000}
+            height={10}
+            alt="news-img"
+            className="rounded-t-xl flex-grow-3  lg:hidden block mb-4"
+          />
+
+          <div className="flex flex-col justify-center">
+            <p className="lg:p-8 px-4  w-full lg:md:text-sm text-xs">
+              Many young Myanmar people, including myself, go abroad to work in
+              order to change their lives...
+            </p>
+            <div className="w-full flex justify-end pb-2">
+              <div className="w-contain py-2">
+                <MoreBtn />
+              </div>
+            </div>
+          </div>
+        </div>
         
        
        
