9bb482dcf0618c4f7609a19745363d9aa294ba46
yoonme44 <yoonmem86@gmail.com>
2023-09-29 08:42:28 +0630

    add responsive layout

--- src/components/CourseOutline.tsx
+++ src/components/CourseOutline.tsx
@@ -7,7 +7,7 @@ const CourseOutline = () => {
       <h2 className="text-2xl text-red-600 font-medium text-center mb-4">
         Course Outline
       </h2>
-      <div className="grid grid-cols-3 gap-[105px] p-4 mx-[100px] justify-center rounded-3xl">
+      <div className="grid lg:grid-cols-3 md:grid-cols-3 lg:gap-[105px] md:gap-[20px] sm:gap-[30px] gap-[30px] p-4 lg:mx-[100px] md:mx-[60px] sm:mx-[150px] mx-[50px] justify-center rounded-3xl">
         <div className="flex-1 p-6 bg-white flex flex-col items-center rounded-2xl shadow-card">
           <div className="rounded-full w-[114px] h-[114px] bg-red-main flex items-center justify-center">
             <Image
@@ -17,7 +17,7 @@ const CourseOutline = () => {
               alt="registered student"
             />
           </div>
-          <label className="md:text-lg text-red-main font-bold mt-4">Japanese Language</label>
+          <label className="md:text-lg text-center text-red-main font-bold mt-4">Japanese Language</label>
           <p className="text-center text-sm font-medium">
             From basic to advanced level, we are teaching to pass the JLPT exam recognized by Japan
           </p>
@@ -32,7 +32,7 @@ const CourseOutline = () => {
               alt="online student"
             />
           </div>
-          <label className="md:text-lg text-red-main font-bold mt-4">Website Design and Coding</label>
+          <label className="md:text-lg text-center text-red-main font-bold mt-4">Website Design and Coding</label>
           <p className="text-sm text-center font-medium">
           It teaches you the basics of designing and coding so that you can design a website yourself
           </p>
@@ -47,7 +47,7 @@ const CourseOutline = () => {
               alt="trainer"
             />
           </div>
-          <label className="md:text-lg text-red-main font-bold mt-4">ITPEC ( IT,FE )</label>
+          <label className="md:text-lg text-center text-red-main font-bold mt-4">ITPEC ( IT,FE )</label>
           <p className="text-sm text-center font-medium">
           We are now teaching to obtain a Japan-accredited computer technician certificate
           </p>
