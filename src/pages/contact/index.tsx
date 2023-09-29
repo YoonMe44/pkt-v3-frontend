7c5c7d508b4558074f7c91038b9b6ca03d65e562
Neroaqucious <hsuhninwai66@gmail.com>
2023-09-29 08:46:44 +0630

    update

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,12 @@
+
+import Contact from "../../components/Contact"; // Rename the imported component
+
+const ContactUsPage = () => {
+  return (
+    <>
+    <Contact />
+    </>
+  );
+}
+
+export default ContactUsPage;
