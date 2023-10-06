0d10c0e77c00bd70d01e66ad6a6469d40ec1ecd1
naingaunglinn <naingaunglinn369@gmail.com>
2023-10-06 09:25:38 +0630

    fix:enroll

--- src/components/Wizard.tsx
+++ src/components/Wizard.tsx
@@ -4,22 +4,17 @@ import Step2 from "./Step2";
 import Step3 from "./Step3";
 import RedButton from "./RedButton";
 import Link from "next/link";
-
 const Wizard: React.FC = () => {
   const [currentStep, setCurrentStep] = useState<number>(1);
-
   const handleNext = () => {
     setCurrentStep(currentStep + 1);
   };
-
   const handlePrev = () => {
     setCurrentStep(currentStep - 1);
   };
-
   const getLineColor = (step: number) => {
     return step <= currentStep ? "bg-red-700" : "bg-gray-300";
   };
-
   return (
     <div>
       <div className="bg-custom-white-17 rounded-[30px] shadow-custom p-[5rem]">
@@ -37,18 +32,18 @@ const Wizard: React.FC = () => {
               <div className="mt-[87px] flex">
                 {step === 1 && (
                   <div className="w-[14rem] text-center">
-                    <p className="font-medium text-[22px]">Course Confirm</p>
+                    <p className="font-medium text-[22px]"> Confirm</p>
                   </div>
                 )}
                 {step === 2 && (
                   <div className="w-[14rem] text-center">
-                    <p className="font-medium text-[22px]">Pay Confirm</p>
+                    <p className="font-medium text-[22px]">Payment</p>
                   </div>
                 )}
                 {step === 3 && (
                   <div className="w-[14rem]">
-                    <p className="font-medium text-[22px]">
-                      Enroll Step Complete
+                    <p className="font-medium text-[22px] text-center">
+                      Complete
                     </p>
                   </div>
                 )}
@@ -70,77 +65,53 @@ const Wizard: React.FC = () => {
           ></div>
         </div>
         {/* Render Current Step */}
-        <div className="width-full  border-[10px]">
+        <div className="w-[75%] rounded-[30px] border-[2px] mx-auto
+         mt-[80px] pt-[40px] pb-[40px]">
           {currentStep === 1 && <Step1 />}
           {currentStep === 2 && <Step2 />}
           {currentStep === 3 && <Step3 />}
-        </div>
-
-        {/* Navigation Buttons */}
-        <div className="mt-4 grid grid-cols-5">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          {currentStep === 1 && (
-            // <button
-            //   className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
-            //   onClick={handleNext}
-            // >
-            //   Next
-            // </button>
-            <div className="w-full mx-auto flex gap-3 justify-end">
-              <button className="WhiteBtn">
-                <span className="shadow1"></span>
-                <span className="edge1"></span>
-                <Link href="#" className="front1">
-                  back
-                </Link>
-              </button>
-              <RedButton title="Continue" onClick={handleNext} />
-            </div>
-          )}
-          {currentStep === 2 && (
-            // <button
-            //   className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
-            //   onClick={handleNext}
-            // >
-            //   Next
-            // </button>
-            <div className="w-full mx-auto flex gap-3 justify-end">
-              <button className="WhiteBtn" onClick={handlePrev}>
-                <span className="shadow1"></span>
-                <span className="edge1"></span>
-                <Link href="#" className="front1">
-                  back
-                </Link>
-              </button>
-              <RedButton title="Continue" onClick={handleNext} />
-            </div>
-          )}
-          {currentStep === 3 && (
-            // <button
-            //   className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
-            //   onClick={handleNext}
-            // >
-            //   Next
-            // </button>
-
-            <div className="w-full mx-auto flex gap-3 justify-end">
-              <button className="WhiteBtn" onClick={handlePrev}>
-                <span className="shadow1"></span>
-                <span className="edge1"></span>
-                <Link href="#" className="front1">
-                  back
-                </Link>
-              </button>
-              <RedButton title="My page" />
-            </div>
-          )}
+          {/* Navigation Buttons */}
+          <div className="mx-auto mt-[40px] w-[80%]">
+            {currentStep === 1 && (
+              <div className="w-full mx-auto flex gap-3 justify-end">
+                <button className="WhiteBtn">
+                  <span className="shadow1"></span>
+                  <span className="edge1"></span>
+                  <Link href="#" className="front1">
+                    back
+                  </Link>
+                </button>
+                <RedButton title="Continue" onClick={handleNext} />
+              </div>
+            )}
+            {currentStep === 2 && (
+              <div className="w-full mx-auto flex gap-3 justify-end">
+                <button className="WhiteBtn" onClick={handlePrev}>
+                  <span className="shadow1"></span>
+                  <span className="edge1"></span>
+                  <Link href="#" className="front1">
+                    back
+                  </Link>
+                </button>
+                <RedButton title="Continue" onClick={handleNext} />
+              </div>
+            )}
+            {currentStep === 3 && (
+              <div className="w-full mx-auto flex gap-3 justify-end">
+                <button className="WhiteBtn" onClick={handlePrev}>
+                  <span className="shadow1"></span>
+                  <span className="edge1"></span>
+                  <Link href="#" className="front1">
+                    back
+                  </Link>
+                </button>
+                <RedButton title="My page" />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Wizard;
