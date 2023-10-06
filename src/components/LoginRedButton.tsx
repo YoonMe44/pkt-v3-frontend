fcf82012f3e909396c3e83638d78556fc0d45531
KyawThu12345 <minkyawt733@gmail.com>
2023-10-06 16:11:53 +0630

    google_login

--- src/components/LoginRedButton.tsx
+++ src/components/LoginRedButton.tsx
@@ -1,32 +1,53 @@
 import Link from "next/link";
+import React from "react";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 
 interface LoginRedButtonProps {
   title?: string;
 }
+
+const GoogleLoginDynamic = dynamic(() => import("react-google-login"), {
+  ssr: false,
+});
+
 const LoginRedButton: React.FC<LoginRedButtonProps> = ({ title }) => {
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  };
   return (
-    <div className="lg:mt-0 md:mt-0 xs:mt-3 sm:mt-3 flex flex-wrap">
+    <div>
       <button className="redBtn">
         <span className="shadow"></span>
         <span className="edge"></span>
         <Link href={"/login"} className="front">
-          <div className="flex items-center justify-center">
-            <div className="inline w-[38px] h-[1px] absolute pb-[39px] mr-[200px] mt-[2px] xs:mr-[200px] s:mr-[158px]">
-              <Image
-                src={"https://img.icons8.com/color/48/google-logo.png"}
-                width={62}
-                height={62}
-                alt="pktlogo"
-                className="mx-auto float-right bg-white rounded-l-[12px]"
-              />
-            </div>
-            <span className="pr-10"></span>
-            {title}
-          </div>
+          <GoogleLoginDynamic
+            clientId="753695289374-b99bdu9aspgrcu6vtpltgc7bhbjhglki.apps.googleusercontent.com"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            render={(props) => (
+              <button
+                onClick={props.onClick}
+                disabled={props.disabled}
+                className="text-white font-bold rounded"
+              >
+                <div className="inline w-[38px] h-[1px] absolute pb-[39px] mr-[200px] mt-[2px] xs:mr-[200px] s:mr-[158px]">
+                  <Image
+                    src={"https://img.icons8.com/color/48/google-logo.png"}
+                    width={62}
+                    height={62}
+                    alt="pktlogo"
+                    className="mx-auto float-right bg-white rounded-l-[12px] mr-[24px] mt-[-11px]"
+                  />
+                </div>
+                <span className="ml-[24px]">Sign up with Google</span>
+              </button>
+            )}
+          />
         </Link>
       </button>
     </div>
   );
 };
+
 export default LoginRedButton;
