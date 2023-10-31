8ae07bf76d1de39737b1d335230ffc89d823b566
PhyoTheinkha04 <kokha042@gmail.com>
2023-10-31 09:41:46 +0630

    instructor detail added

--- src/components/MoreBtn.tsx
+++ src/components/MoreBtn.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
-
-const MoreBtn = () => {
-    return (
-      <div className="w-[100px] mx-auto mt-4">
-        <Link href="/news/1" className="cta">
-          <span>More</span>
-          <svg viewBox="0 0 13 10" height="10px" width="15px">
-            <path d="M1,5 L11,5"></path>
-            <polyline points="8 1 12 5 8 9"></polyline>
-          </svg>
-        </Link>
-      </div>
-    );
+interface MoreBtnProps {
+  link?: string;
+  onClick?: () => void;
 }
+const MoreBtn: React.FC<MoreBtnProps> = ({  link, onClick }) => {
+  return (
+    <div className="w-[100px] mx-auto mt-4">
+      <Link href={link || "#"} className="cta">
+        <span>More</span>
+        <svg viewBox="0 0 13 10" height="10px" width="15px">
+          <path d="M1,5 L11,5"></path>
+          <polyline points="8 1 12 5 8 9"></polyline>
+        </svg>
+      </Link>
+    </div>
+  );
+};
 export default MoreBtn;
